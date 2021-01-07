2a19d2303b95a4e9a09962f35c1d62105d77a6d2
devMaryMelo <marymelomafvm@gmail.com>
2021-01-07 08:36:23 -0300

    Update TelaLogin.java

--- src/br/edu/iftm/views/TelaLogin.java
+++ src/br/edu/iftm/views/TelaLogin.java
@@ -21,16 +21,16 @@ public class TelaLogin extends PanelPrincipal
     public TelaLogin(JPanel telas, JFrame janela)
     {
         super(telas, janela);
-        JLabel lblNome = new JLabel("Nome");
-        lblNome.setForeground(Color.WHITE);
-        lblNome.setBounds(630, 300, 100, 20);
+        JLabel lblUsuario = new JLabel("Usuário");
+        lblUsuario.setForeground(Color.WHITE);
+        lblUsuario.setBounds(630, 300, 100, 20);
 
         JLabel lblSenha = new JLabel("Senha");
         lblSenha.setForeground(Color.WHITE);
         lblSenha.setBounds(630, 350, 100, 20);
 
-        JTextField txtNome = new JTextField();
-        txtNome.setBounds(630, 320, 200, 20);
+        JTextField txtUsuario = new JTextField();
+        txtUsuario.setBounds(630, 320, 200, 20);
 
         JPasswordField password = new JPasswordField();
         password.setBounds(630, 370, 200, 20);
@@ -44,41 +44,41 @@ public class TelaLogin extends PanelPrincipal
         Imagem imgCoffee = new Imagem("coffee.png");
         imgCoffee.setBounds(5, 5, 64, 64);
         
-        this.add(lblNome);
-        this.add(txtNome);
+        this.add(lblUsuario);
+        this.add(txtUsuario);
         this.add(lblSenha);
         this.add(password);
         this.add(botaoLogin);
         this.add(imgCafeteria);
         this.add(imgCoffee);
 
-    botaoLogin.addActionListener(new ActionListener()
-    {
+        botaoLogin.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String usuario = txtUsuario.getText();
+                String senha = new String(password.getPassword());
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String nome = txtNome.getText();
-            String senha = new String(password.getPassword());
-
-            if((nome == null) || (nome.isEmpty()) || (senha.isEmpty()))
-            {
-                JOptionPane.showMessageDialog(null, "Informe o nome e a senha", "Dados inválidos", 
-                                                JOptionPane.ERROR_MESSAGE);
-            }
-            else
-            {
-                if((nome.equals("sleepy_dev")) && (senha.equals("coffee")))
+                if((usuario == null) || (usuario.isEmpty()) || (senha.isEmpty()))
                 {
-                    trocarTela("Principal");
+                    JOptionPane.showMessageDialog(null, "Informe o usuario e a senha", "Dados inválidos", 
+                                                    JOptionPane.ERROR_MESSAGE);
+                }
+                else
+                {
+                    if((usuario.equals("sleepy_dev")) && (senha.equals("coffee")))
+                    {
+                        trocarTela("Principal");
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos", "Dados inválidos", 
+                                                    JOptionPane.ERROR_MESSAGE);
+                    }
                 }
             }
-        }
-
-    });
-}
+        });
+    }
 }
 
-   /* public void executarBotao(ActionEvent e)
-    {
-        trocarTela("Principal");
-    }*/
+    
