688c24d01b7c103506a71cf062720de7c678bdca
devMaryMelo <marymelomafvm@gmail.com>
2021-01-08 17:54:28 -0300

    Update TelaPrincipal.java

--- src/br/edu/iftm/views/TelaPrincipal.java
+++ src/br/edu/iftm/views/TelaPrincipal.java
@@ -1,7 +1,12 @@
 package br.edu.iftm.views;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import br.edu.iftm.views.components.Imagem;
 import br.edu.iftm.views.components.PanelPrincipal;
@@ -13,12 +18,13 @@ import java.awt.Cursor;
 public class TelaPrincipal extends PanelPrincipal {
     private JPanel telas;
     private CardLayout controlador;
+    private static JFrame cardapio;
 
     public TelaPrincipal(JPanel telas, JFrame janela) {
         super(telas, janela);
 
         this.setLayout(null);
-        
+
         Imagem background = new Imagem("background.jpg");
         background.setBounds(0, 0, 1440, 900);
 
@@ -34,29 +40,44 @@ public class TelaPrincipal extends PanelPrincipal {
 
         Imagem imgCardapio = new Imagem("imgCardapio.png");
         imgCardapio.setBounds(0, 0, 1545, 2000);
-        
+
+        JTextField txtProduto = new JTextField();
+        txtProduto.setBounds(500, 400, 200, 20);
+
+        JTextField txtPreco = new JTextField();
+        txtPreco.setBounds(500, 450, 200, 20);
+
         this.add(btnCardapio);
         this.add(btnLogout);
         this.add(background);
+        this.add(txtProduto);
+        this.add(txtPreco);
 
-        btnLogout.addMouseListener(new MouseAdapter() 
-        {
-            public void mouseClicked(MouseEvent e) 
-            {
+        btnLogout.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
                 trocarTela("Login");
             }
         });
 
-        btnCardapio.addMouseListener(new MouseAdapter() 
-        {
-            public void mouseClicked(MouseEvent e) 
-            {
-                JFrame janela = new JFrame("Cardápio");
-                janela.setBounds(70, 50, 415, 550);
-                janela.add(imgCardapio);
-                janela.setResizable(false);
-                janela.setVisible(true);
+        btnCardapio.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                cardapio = new JFrame("Cardápio");
+                cardapio.setBounds(70, 50, 415, 550);
+                cardapio.add(imgCardapio);
+                cardapio.setResizable(false);
+                cardapio.setVisible(true);
             }
         });
     }
+
+    public static JFrame getCardapio() 
+    {
+        return cardapio;
+    }
+
+    public void setCardapio(JFrame cardapio) 
+    {
+        TelaPrincipal.cardapio = cardapio;
+    }
+
 }
