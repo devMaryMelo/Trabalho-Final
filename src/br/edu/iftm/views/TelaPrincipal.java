b67ab3d4235ba1a06d5223b04b838d98813ee6bd
devMaryMelo <marymelomafvm@gmail.com>
2021-01-07 23:15:19 -0300

    Update TelaPrincipal.java

--- src/br/edu/iftm/views/TelaPrincipal.java
+++ src/br/edu/iftm/views/TelaPrincipal.java
@@ -1,8 +1,10 @@
 package br.edu.iftm.views;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
 
 import br.edu.iftm.views.components.Imagem;
 import br.edu.iftm.views.components.PanelPrincipal;
@@ -10,18 +12,33 @@ import br.edu.iftm.views.components.PanelPrincipal;
 import java.awt.event.*;
 import java.awt.CardLayout;
 import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Color;
 
-public class TelaPrincipal extends PanelPrincipal
+public class TelaPrincipal extends PanelPrincipal 
 {
     private JPanel telas;
     private CardLayout controlador;
 
-    public TelaPrincipal(JPanel telas, JFrame janela)
-    {
+    public TelaPrincipal(JPanel telas, JFrame janela) {
         super(telas, janela);
 
-        Imagem btnLogout = new Imagem("logout.png");
-        btnLogout.setBounds(10, 0, 64, 64);
+        this.setLayout(null);
+
+        String [] colunas = {"Nome", "Idade", "Genero"};
+        
+        String[][] dados = {{"Maria", "19", "Feminino"},
+                            {"Jose", "20", "Masculino"}};
+        
+        JTable table = new JTable(dados, colunas);
+        table.setBounds(15, 150, 270, 500);
+        table.setBackground(Color.decode("#DEB887"));
+
+        Imagem background = new Imagem("background.png");
+        background.setBounds(0, 0, 1920, 1280);
+
+        Imagem btnLogout = new Imagem("logoutwhite.png");
+        btnLogout.setBounds(7, 0, 64, 64);
         btnLogout.setToolTipText("Logout");
         btnLogout.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
@@ -32,6 +49,9 @@ public class TelaPrincipal extends PanelPrincipal
                 trocarTela("Login");
             }
         });
+        
         this.add(btnLogout);
+        this.add(table);
+        this.add(background);
     }
 }
