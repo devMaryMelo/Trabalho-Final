e243adada9f8a80a5518e9f590184f4e9d41eed9
devMaryMelo <marymelomafvm@gmail.com>
2021-01-08 23:48:07 -0300

    Update Botao.java

--- src/br/edu/iftm/views/components/Botao.java
+++ src/br/edu/iftm/views/components/Botao.java
@@ -8,8 +8,10 @@ public class Botao extends JButton
     public Botao(String texto)
     {
         super(texto);
-        this.setBounds(683, 450, 70, 30);
-        this.setBackground(Color.decode("#6d5dd3"));
+        this.setSize(70, 30);
+        this.setLocation(683, 450);
+        this.setBackground(Color.decode("#581845"));
+        this.setForeground(Color.decode("#FEFEFE"));
         this.setFocusPainted(false);
         this.setBorderPainted(false);
     }
