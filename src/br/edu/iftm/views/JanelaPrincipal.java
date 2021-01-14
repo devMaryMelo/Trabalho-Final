87e4a3472bf30e2f003dc23e2a7ea4627245a4c2
devMaryMelo <marymelomafvm@gmail.com>
2021-01-14 18:20:41 -0300

    Update JanelaPrincipal.java

--- src/br/edu/iftm/views/JanelaPrincipal.java
+++ src/br/edu/iftm/views/JanelaPrincipal.java
@@ -7,26 +7,36 @@ import java.awt.CardLayout;
 
 public class JanelaPrincipal extends JFrame
 {
+    //Método construtor
     public JanelaPrincipal()
     {
+        //Passa as informações da janela
         this.setBounds(330, 100, 720, 480);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setUndecorated(true);
 
+        //Cria um cardlayout para trocar as telas
         CardLayout controlador = new CardLayout();
+        
+        //Cria um JPanel e adiciona o cardlayout nele
         JPanel telas = new JPanel(controlador);
 
+        //Instância os objetos e passa os parâmetros do construtor
         TelaLogin telaLogin = new TelaLogin(telas, this);
         TelaPrincipal telaPrincipal = new TelaPrincipal(telas, this);
 
+        //Adiciona as telas no JPanel
         telas.add(telaLogin, "Login");
         telas.add(telaPrincipal, "Principal");
 
+        //Define com qual tela o programa iniciará
         controlador.show(telas, "Login");
 
+        //Adiciona as telas na janela
         this.add(telas);
 
+        //Define a janela como visível 
         this.setVisible(true);
     }
 }
